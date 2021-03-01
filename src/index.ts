86b2c4ede1e20d226a353a0045546d95d002248b
Luis Angel <luisangelpenacemas@gmail.com>
2021-02-28 23:39:33 -1200

    Actualizar data en firestore

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,28 @@ const usuario = {
     fechanac: 0
 }
 
+const usuarioRef = db.collection('usuario');
+
 // insert into usuarios ....
 
 // db.collection('usuario')
 // .add(usuario)
+
+//update
+
+//usuarioRef
+// .doc('BdS7f346XkMsGPVWFJbc')
+// .update({
+ //  activo: true
+//})
+
+usuarioRef
+.doc('BdS7f346XkMsGPVWFJbc')
+.set({
+   activo: true,
+   edad: 15,
+   id: 566,
+})
+
+
+
