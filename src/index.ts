e6b5f74c2c84e3e3a98b062d9def572cfd6b06f7
Luis Angel <luisangelpenacemas@gmail.com>
2021-03-02 19:07:15 -1200

    orderBy

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,16 @@
 import db from './firebase/config';
 import {retornaDocumentos} from './helpers/mostrar-documentos';
 const usuario = {
-    nombre:'Marbelis',
-    activo:true,
-    fechanac: 0
+    nombre:'Sintia',
+    activo:false,
+    fechanac: 0,
+    salario: 6000
 }
+
 
 const usuarioRef = db.collection('usuario');
 
-// insert into usuarios ....
-
-// db.collection('usuario')
-// .add(usuario)
-
-//update
-
-//usuarioRef
-// .doc('BdS7f346XkMsGPVWFJbc')
-// .update({
- //  activo: true
-//})
-
-//destructivo
-// usuarioRef
- // .doc('BdS7f346XkMsGPVWFJbc')
-// .set({
- //  activo: true,
-  // edad: 15,
-  // id: 566,
-// })
-
-
-//delete
-
-// usuarioRef
-// .doc('ko1PSYaoQVXSoIxpHUMW')
-// .delete()
-//.then( () => console.log('borrado') )
-// .catch(e => console.log('error', e) );
-
-// select * from usuario;
-//usuarioRef
-//.onSnapshot(retornaDocumentos)
-
-usuarioRef.where('salario','>=',1500)
-
-.where('activo', '==',true)
-
-.get().then(retornaDocumentos);
-
-
-
-
-
+usuarioRef.orderBy('nombre')
+.orderBy('salario')
+    .get().then(retornaDocumentos)
 
